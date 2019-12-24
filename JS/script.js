6967649be0af93257ceaaa553963724e1af92ed1
rw123456789 <rosalie_wood@hotmail.com>
2019-12-24 12:30:28 +0000

    chase button
    new game jquery chase

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+var caller = $("emcee"),
+    box = $("#box");
+
+function foo(){
+  var randX = Math.floor(Math.random() * (window.innerWidth - 100));
+  var randY = Math.floor(Math.random() * (window.innerHeight - 100));
+  console.log([randX, randY]);
+  caller.stop().animate({"left": randX + "px", "top": randY + "px"});
+}
+
+$(document).ready(function() {
+  caller.on('mouseenter', foo);
+  caller.on('click', function(){
