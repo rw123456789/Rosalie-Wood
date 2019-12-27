64ef0e4e6ed0d905e0763c0cba3fb7d0f06d47c9
rw123456789 <rosalie_wood@hotmail.com>
2019-12-27 18:52:45 +0000

    Script new game update experiment

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,15 @@
+$('.touchMeNot').on('mouseenter',function(e){
+    var maxX = $(window).width() - $(this).width();
+    var maxY = $(window).height() - $(this).height();
+    $(this).css({
+        'left':getRandomInt(0, maxX),
+        'top':getRandomInt(0, maxY)
+    });
+});
+funtion getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 var caller = $("emcee"),
     box = $("#box");
 
