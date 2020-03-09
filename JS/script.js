551b4a0383b9a81a1f72e2b908d20166aa09bc48
rw123456789 <rosalie_wood@hotmail.com>
2020-03-09 22:38:28 +0000

    clean up CD JS

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,18 @@
-$('.touchMeNot').on('mouseenter',function(e){
-    var maxX = $(window).width() - $(this).width();
-    var maxY = $(window).height() - $(this).height();
-    $(this).css({
-        'left':getRandomInt(0, maxX),
-        'top':getRandomInt(0, maxY)
-    });
-});
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-var caller = $("emcee"),
-    box = $("#box");
-
-function foo(){
-  var randX = Math.floor(Math.random() * (window.innerWidth - 100));
-  var randY = Math.floor(Math.random() * (window.innerHeight - 100));
-  console.log([randX, randY]);
-  caller.stop().animate({"left": randX + "px", "top": randY + "px"});
-}
-
 $(document).ready(function() {
-  caller.on('mouseenter', foo);
-  caller.on('click', function(){
-  alert('clicked!');
+  var touchMeNot = $(".touch-me-not");
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  touchMeNot.on("mouseenter", () => {
+    var randX = Math.floor(Math.random() * (window.innerWidth - 100));
+    var randY = Math.floor(Math.random() * (window.innerHeight - 100));
+
+    touchMeNot.stop().animate({ left: randX + "px", top: randY + "px" });
   });
 
+  touchMeNot.on("click", function() {
+    alert("clicked!");
+  });
 });
